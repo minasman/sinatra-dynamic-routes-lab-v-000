f610b3bc76b8296623477e019021342520bb2149
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-07-21 13:21:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,10 +15,7 @@ end
 get '/say/:number/:phrase' do
   num = params[:number].to_i
   phrase = params[:phrase]
-  while num > 0
-    "#{phrase}"
-    num = num - 1 
-  end
+
 end
 
 get '/say/:word1/:word2/:word3/:word4/:word5' do
