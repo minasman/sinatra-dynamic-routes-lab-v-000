959974136224abd8258e880a180e9f9bfd64fa9d
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-07-21 13:23:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,9 +13,12 @@ get '/square/:number' do
 end
 
 get '/say/:number/:phrase' do
-  num = params[:number].to_i
-  phrase = params[:phrase]
-
+  @number = params[:number].to_i
+  @phrase = params[:phrase]
+  @all = []
+  @number.times do
+    end
+    @all.join
 end
 
 get '/say/:word1/:word2/:word3/:word4/:word5' do
